fe06c1b921be67c6b0e859d893ea0999beefc914
vmbg00 <vmbg00@gmail.com>
2021-02-19 21:00:46 +0200

    Added repositories.

---
+++ src/main/java/bg/softuni/andreys/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package bg.softuni.andreys.repositories;
+
+import bg.softuni.andreys.models.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
